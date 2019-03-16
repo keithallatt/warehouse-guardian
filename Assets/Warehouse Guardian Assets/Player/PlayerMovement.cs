31d3609449592c41a6cea5411f6b032f52506665
Raghav Dua <raghavdua1999@gmail.com>
2019-03-16 01:38:43 -0400

    player moving
    added a movable player, with other small edits to organization

---
+++ Assets/Warehouse Guardian Assets/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody2D rbody;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>(); //allows us to access the animator that was attached to player
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); //returns us true or false, ex if we press "A" key horizontal will become -1
+
+        if (movement_vector != Vector2.zero)
+        {
+            anim.SetBool("iswalking", true);
+            anim.SetFloat("input_x", movement_vector.x); //we set these two so that it keeps facing the last direction
+            anim.SetFloat("input_y", movement_vector.y);
+        }
+        else
+        {
+            anim.SetBool("iswalking", false);
+        }
+
+        rbody.MovePosition(rbody.position + movement_vector * Time.deltaTime); //moves 1m per frame
+    }
+}
