93385c70fa252b02c479d6899d8ff7b3102f1c3a
Raghav Dua <raghavdua1999@gmail.com>
2019-03-17 01:55:52 -0400

    player
    player movement and some other oganization tweaks

--- Assets/Warehouse Guardian Assets/Player/PlayerMovement.cs
+++ Assets/Warehouse Guardian Assets/Player/PlayerMovement.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     Rigidbody2D rbody;
     Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        rbody = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>(); //allows us to access the animator that was attached to player
+        rbody = GetComponent<Rigidbody2D>(); //gets the components that are attached to the player in unity
+        anim = GetComponent<Animator>(); //gets the components that are attached to the player in unity
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); //returns us true or false, ex if we press "A" key horizontal will become -1
+        Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); //returns us true or false. ex pressing left makes horz -1 and releasing makes it 0
 
         if (movement_vector != Vector2.zero)
         {
             anim.SetBool("iswalking", true);
-            anim.SetFloat("input_x", movement_vector.x); //we set these two so that it keeps facing the last direction
+            anim.SetFloat("input_x", movement_vector.x);
             anim.SetFloat("input_y", movement_vector.y);
         }
         else
@@ -30,6 +30,6 @@ public class PlayerMovement : MonoBehaviour
             anim.SetBool("iswalking", false);
         }
 
-        rbody.MovePosition(rbody.position + movement_vector * Time.deltaTime); //moves 1m per frame
+        rbody.MovePosition (rbody.position + movement_vector * Time.deltaTime); //moving over 1 meter per 60 frames
     }
 }
